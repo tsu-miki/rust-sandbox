803cfc8af91e441ca1b1abcd93af19df44ad4214
tsu-miki <wimps.r0930@icloud.com>
2026-06-01 21:24:38 +0900

    clone

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,7 @@
+use std::thread::scope;
+
 fn main() {
-    let x = 5;
-    let y = x;
-    println!("x={x}, y={y}");
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1={s1}, s2={s2}")
 }
